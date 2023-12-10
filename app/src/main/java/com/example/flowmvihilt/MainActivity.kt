4e7ebf5e53305f57126ce95a2a094d8fb6985033
libo <417845639@qq.com>
2023-12-10 18:45:45 +0800

    NavhostFragment使用

--- app/src/main/java/com/example/flowmvihilt/MainActivity.kt
+++ app/src/main/java/com/example/flowmvihilt/MainActivity.kt
@@ -1,51 +1,59 @@
 package com.example.flowmvihilt
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.flowmvihilt.base.BaseBindingActivity
 import com.example.flowmvihilt.databinding.ActivityMainBinding
-import com.example.flowmvihilt.ui.main.MainFragment
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.ServiceLoader
 
 @AndroidEntryPoint
-class MainActivity : BaseBindingActivity<ActivityMainBinding>(
-    {ActivityMainBinding.inflate(it)}
-) {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding.navView.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.navigation_home -> switchFragment(0)
-                R.id.navigation_qa -> switchFragment(1)
-                R.id.navigation_system -> switchFragment(2)
-                R.id.navigation_mine -> switchFragment(3)
-            }
-            true
-        }
+        setContentView(R.layout.activity_main)
+
+
+        //将Navigation与BNV关联
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+//        val navController = navHostFragment.findNavController()
+//        binding.navView.setupWithNavController(navController)
+
+//        binding.navView.setOnItemSelectedListener {
+//            when (it.itemId) {
+//                R.id.navigation_home -> switchFragment(0)
+//                R.id.navigation_qa -> switchFragment(1)
+//                R.id.navigation_system -> switchFragment(2)
+//                R.id.navigation_mine -> switchFragment(3)
+//            }
+//            true
+//        }
 
         initViewPager()
     }
 
-    private fun switchFragment(position: Int) = binding.mainViewPager.setCurrentItem(position, true)
+//    private fun switchFragment(position: Int) = binding.mainViewPager.setCurrentItem(position, true)
 
     private fun initViewPager() {
-        binding.mainViewPager.isUserInputEnabled = false
-        binding.mainViewPager.offscreenPageLimit = 2
-        binding.mainViewPager.adapter = object : FragmentStateAdapter(this) {
-            override fun getItemCount(): Int = 5
-
-            override fun createFragment(position: Int): Fragment = when (position) {
-                0 -> MainFragment()
-                1 -> MainFragment()
-                2 -> MainFragment()
-                3 -> MainFragment()
-                else -> MainFragment()
-            }
-
-        }
+//        binding.mainViewPager.isUserInputEnabled = false
+//        binding.mainViewPager.offscreenPageLimit = 2
+//        binding.mainViewPager.adapter = object : FragmentStateAdapter(this) {
+//            override fun getItemCount(): Int = 5
+//
+//            override fun createFragment(position: Int): Fragment = when (position) {
+//                0 -> MainFragment()
+//                1 -> MainFragment()
+//                2 -> MainFragment()
+//                3 -> MainFragment()
+//                else -> MainFragment()
+//            }
+//
+//        }
     }
 }
