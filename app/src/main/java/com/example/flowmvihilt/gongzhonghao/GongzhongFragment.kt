b36641cd098cf232253b8718797552c6292ec24d
libo <417845639@qq.com>
2024-01-04 11:27:48 +0800

    公众号tab数据添加

--- app/src/main/java/com/example/flowmvihilt/gongzhonghao/GongzhongFragment.kt
+++ app/src/main/java/com/example/flowmvihilt/gongzhonghao/GongzhongFragment.kt
@@ -2,18 +2,34 @@ package com.example.flowmvihilt.gongzhonghao
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import com.example.basemodule.basemvi.BaseBindingFragment
 import com.example.flowmvihilt.databinding.FragmentGongzhongBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.ArrayList
 
 @AndroidEntryPoint
 class GongzhongFragment : BaseBindingFragment<FragmentGongzhongBinding>({
     FragmentGongzhongBinding.inflate(it)
 }) {
 
+    private val fragments = ArrayList<Fragment>()
+    private var pagerAdapter: CommPagerAdapter? = null
+    private val titles = arrayOf("关注 128", "粉丝 128", "推荐关注","关注 128", "粉丝 128", "推荐关注","关注 128", "粉丝 128", "推荐关注")
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        init()
+    }
 
+    fun init() {
+        for (i in titles.indices) {
+            fragments.add(GongzhonghaoPageFragment())
+            binding.tablayout.addTab(binding.tablayout.newTab().setText(titles[i]))
+        }
+        pagerAdapter = CommPagerAdapter(childFragmentManager, fragments, titles)
+        binding.viewpager.adapter = pagerAdapter
+        binding.tablayout.setupWithViewPager(binding.viewpager)
     }
 }
---
+++ app/src/main/java/com/example/flowmvihilt/gongzhonghao/GongzhonghaoPageFragment.kt
@@ -0,0 +1,21 @@
+package com.example.flowmvihilt.gongzhonghao
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+
+class GongzhonghaoPageFragment: Fragment() {
+
+    companion object {
+        val recyclerViewPool = RecyclerView.RecycledViewPool()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+    fun init() {
+    }
+}
